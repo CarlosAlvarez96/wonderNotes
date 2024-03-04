8e58c56791f080bf6b07b3020c999b1054b9e01a
CarlosAlvarez96 <calvarezmartin96@gmail.com>
2024-03-04 21:24:20 +0100

    1.0

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
 
   return (
     <div>
-      <footer className={`flex flex-row justify-around p-4 ${themeClass}`}>
-        <h2 className="text-2xl font-bold p-2 m-2">
+      <footer className={`flex flex-col md:flex-row justify-around p-4 ${themeClass}`}>
+        <h2 className="text-2xl font-bold p-2 m-2 md:mb-0 md:mr-4">
           Proyecto para Horas de libre configuración
         </h2>
         <h2 className="text-2xl font-bold p-2 m-2">Carlos Álvarez Martín</h2>
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -9,26 +9,32 @@ const Header = () => {
   const { themeClass, toggleDarkMode } = useTheme();
   const navigate = useNavigate();
 
+  const [menuOpen, setMenuOpen] = useState(false);
   const [hoverClass, setHoverClass] = useState("");
 
   const handleLogin = () => {
     navigate("/login");
+    closeMenu();
   };
 
   const handleLogout = () => {
     logout();
+    closeMenu();
   };
 
   const handleRegister = () => {
     navigate("/register");
+    closeMenu();
   };
 
   const handleNotes = () => {
     navigate("/");
+    closeMenu();
   };
 
   const handleAddNote = () => {
     navigate("/noteForm");
+    closeMenu();
   };
 
   const toggleHoverClass = () => {
@@ -41,17 +47,46 @@ const Header = () => {
     setHoverClass("");
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <div className={`bg-${themeClass}`}>
-      <div className="flex flex-row justify-evenly col-span-6 p-4 border-b-2">
-        <h2 className={`text-4xl font-bold p-2 m-2 ${themeClass}`}>
-          WonderNotes✒️
-        </h2>
-        <nav>
-          <ul className="flex flex-row justify-between p-4">
+      <div className="flex flex-col md:flex-row justify-between items-center p-4 border-b-2">
+        <div className="flex items-center">
+          <h2 className={`text-4xl font-bold p-2 m-2 ${themeClass}`}>
+            WonderNotes✒️
+          </h2>
+        </div>
+        <button
+          className="md:hidden focus:outline-none"
+          onClick={toggleMenu}
+        >
+          <svg
+            className={`w-6 h-6 text-${themeClass} hover:text-${hoverClass}`}
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
+        <nav className={`md:flex md:items-center md:w-auto ${menuOpen ? 'block' : 'hidden'}`}>
+          <ul className="flex flex-col md:flex-row justify-between space-y-2 md:space-y-0 md:space-x-4 mt-4 md:mt-0">
             <button
               onClick={toggleDarkMode}
-              className={`bg-${themeClass}  font-bold px-4 py-2 rounded`}
+              className={`bg-${themeClass} font-bold px-4 py-2 rounded`}
               onMouseEnter={toggleHoverClass}
               onMouseLeave={resetHoverClass}
             >
@@ -61,7 +96,7 @@ const Header = () => {
             </button>
             <button
               onClick={handleNotes}
-              className={`p-2 m-2 rounded-lg hover:bg-${hoverClass}`}
+              className={`p-2 rounded-lg hover:bg-${hoverClass}`}
               onMouseEnter={toggleHoverClass}
               onMouseLeave={resetHoverClass}
             >
@@ -69,7 +104,7 @@ const Header = () => {
             </button>
             <button
               onClick={handleAddNote}
-              className={`p-2 m-2 rounded-lg hover:bg-${hoverClass}`}
+              className={`p-2 rounded-lg hover:bg-${hoverClass}`}
               onMouseEnter={toggleHoverClass}
               onMouseLeave={resetHoverClass}
             >
@@ -79,7 +114,7 @@ const Header = () => {
               <div>
                 <button
                   onClick={handleLogin}
-                  className={`p-2 m-2 rounded-lg hover:bg-${hoverClass}`}
+                  className={`p-2 rounded-lg hover:bg-${hoverClass}`}
                   onMouseEnter={toggleHoverClass}
                   onMouseLeave={resetHoverClass}
                 >
@@ -87,7 +122,7 @@ const Header = () => {
                 </button>
                 <button
                   onClick={handleRegister}
-                  className={`p-2 m-2 rounded-lg hover:bg-${hoverClass}`}
+                  className={`p-2 rounded-lg hover:bg-${hoverClass}`}
                   onMouseEnter={toggleHoverClass}
                   onMouseLeave={resetHoverClass}
                 >
@@ -98,7 +133,7 @@ const Header = () => {
             {token && (
               <button
                 onClick={handleLogout}
-                className={`p-2 m-2 rounded-lg hover:bg-${hoverClass}`}
+                className={`p-2 rounded-lg hover:bg-${hoverClass}`}
                 onMouseEnter={toggleHoverClass}
                 onMouseLeave={resetHoverClass}
               >
